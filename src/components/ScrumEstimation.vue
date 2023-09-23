627f91b3bdbdd3dad40ca6aa9fbe20032d5bc830
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2023-09-23 22:48:51 +0200

    initial commit exercise2

---
+++ src/components/ScrumEstimation.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h1 class="headline">Headline</h1>
+    <div :title="bodyTitle">{{ bodyText }} (hover to see tooltip)</div>
+    <div>Concatenated: {{ titleAndText }}</div>
+    <button @click="changeText('some other text')">Click me</button>
+    <button @click="changeText('even more text')">Click me also</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+
+const bodyText = ref("Some text");
+const bodyTitle = ref("Body title");
+
+const changeText = (value: string): void => {
+  bodyText.value = value;
+};
+
+const titleAndText = computed(
+  (): string => bodyText.value + " " + bodyTitle.value
+);
+</script>
+
+<style scoped lang="scss">
+.headline {
+  color: red;
+  font-size: 20px;
+}
+
+.container {
+  text-align: center;
+}
+</style>
