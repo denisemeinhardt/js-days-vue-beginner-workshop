9a7745fdf18d8bc2255ac3b492030d286802e557
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2023-09-23 22:50:26 +0200

    initial commit exercise4

---
+++ src/components/ScrumEstimationCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="awesomescrumestimation-card"
+    :class="
+      props.selectedValue === props.value
+        ? 'awesomescrumestimation-card--selected'
+        : ''
+    "
+    @click="emitSelected"
+  >
+    {{ props.value }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  value: string;
+  selectedValue?: string;
+}>();
+
+const emit = defineEmits<{
+  (event: "selected", value: string): void;
+}>();
+
+const emitSelected = () => {
+  emit("selected", props.value);
+};
+</script>
+
+<style lang="scss" scoped>
+@import "src/style/scrumestimation_card";
+</style>
