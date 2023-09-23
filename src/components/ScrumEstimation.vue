e94d39c60596390a28e790d9adabe37b66d845a5
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2023-09-23 22:43:10 +0200

    initial commit exercise 1

---
+++ src/components/ScrumEstimation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1 class="headline">Headline</h1>
+    <div>{{ divText }}</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const divText = "someText";
+</script>
+
+<style scoped lang="scss">
+.headline {
+  color: red;
+  font-size: 20px;
+}
+
+.container {
+  text-align: center;
+}
+</style>
