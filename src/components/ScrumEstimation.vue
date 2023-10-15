667cf9fafe8efaae60629ad0a3fd58a88b4e10b7
Eike <eike@denise.com>
2023-10-15 14:50:33 +0200

    Add solution for exercise 3

--- src/components/ScrumEstimation.vue
+++ src/components/ScrumEstimation.vue
@@ -1,29 +1,47 @@
 <template>
   <div class="container">
-    <h1 class="funnyHeadline">This is fun</h1>
-    <div class="funnyText" :title="anotherFunnyText">
-      {{ funnyText }}
+    <h1 class="funnyHeadline">Planning poker</h1>
+    <div :title="anotherFunnyText">
+      <li
+        v-for="value in selectedPoker"
+        :key="value"
+        @click="setSelectedCard(value)"
+        class="awesomescrumestimation-card"
+        :class="{ 'awesomescrumestimation-card--selected': isSelected(value) }"
+      >
+        {{ value }}
+      </li>
     </div>
-    <button @click="resetText('lustiger')">Change funny text</button>
-    <div>
-      {{ anotherFunnyText + " - " + funnyText }}
-    </div>
-    <button @click="resetText('unlustiger')">
-      Change funny text differently
-    </button>
+    <button @click="resetCards()">Reset cards</button>
+    <select v-model="selectedPoker">
+      <option :value="scrumEstimationValues.fibonacci">Fibonacci</option>
+      <option :value="scrumEstimationValues.shirtSize">Tshirt sizes</option>
+    </select>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
-const funnyText = ref<string>("this even more");
+import scrumEstimationValues from "@/services/scrumEstimationValuesProvider";
 const anotherFunnyText = ref<string>("hello again");
-const resetText = (insertParameterHere: string) => {
-  funnyText.value = insertParameterHere + " wirds nicht mehr";
+const resetCards = () => {
+  selectedCard.value = undefined;
 };
+const selectedCard = ref<string>();
+const setSelectedCard = (value: string) => {
+  selectedCard.value = value;
+};
+const isSelected = (value: string) => {
+  if (value === selectedCard.value) {
+    return true;
+  }
+  return false;
+};
+const selectedPoker = ref(scrumEstimationValues.fibonacci);
 </script>
 
 <style lang="scss" scoped>
+@import "../style/_scrumestimation_card.scss";
 .funnyHeadline {
   color: red;
   font-size: 20px;
