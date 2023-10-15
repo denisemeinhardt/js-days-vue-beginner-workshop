acaa583d17ec061892520d87a58e0775ea94ebc7
Eike <eike@denise.com>
2023-10-15 14:51:16 +0200

    Add solution for exercise 4

--- src/components/ScrumEstimation.vue
+++ src/components/ScrumEstimation.vue
@@ -2,15 +2,13 @@
   <div class="container">
     <h1 class="funnyHeadline">Planning poker</h1>
     <div :title="anotherFunnyText">
-      <li
+      <ScrumEstimationCard
         v-for="value in selectedPoker"
         :key="value"
-        @click="setSelectedCard(value)"
-        class="awesomescrumestimation-card"
-        :class="{ 'awesomescrumestimation-card--selected': isSelected(value) }"
-      >
-        {{ value }}
-      </li>
+        :card-value="value"
+        :selected-card-value="selectedCard"
+        @my-event="selectCard"
+      />
     </div>
     <button @click="resetCards()">Reset cards</button>
     <select v-model="selectedPoker">
@@ -22,20 +20,15 @@
 
 <script lang="ts" setup>
 import { ref } from "vue";
+import ScrumEstimationCard from "./ScrumEstimationCard.vue";
 import scrumEstimationValues from "@/services/scrumEstimationValuesProvider";
 const anotherFunnyText = ref<string>("hello again");
+const selectedCard = ref<string>();
+const selectCard = (card: string) => {
+  selectedCard.value = card;
+};
 const resetCards = () => {
   selectedCard.value = undefined;
-};
-const selectedCard = ref<string>();
-const setSelectedCard = (value: string) => {
-  selectedCard.value = value;
-};
-const isSelected = (value: string) => {
-  if (value === selectedCard.value) {
-    return true;
-  }
-  return false;
 };
 const selectedPoker = ref(scrumEstimationValues.fibonacci);
 </script>
---
+++ src/components/ScrumEstimationCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    @click="setSelectedCard(props.cardValue)"
+    class="awesomescrumestimation-card"
+    :class="{
+      'awesomescrumestimation-card--selected': isSelected(props.cardValue),
+    }"
+  >
+    {{ props.cardValue }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  cardValue: string;
+  selectedCardValue?: string;
+}>();
+const emit = defineEmits<{ (e: "my-event", value: string): void }>();
+const setSelectedCard = (value: string) => {
+  emit("my-event", value);
+};
+const isSelected = (value: string) => {
+  if (value === props.selectedCardValue) {
+    return true;
+  }
+  return false;
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/_scrumestimation_card.scss";
+</style>
