9502399e4ccc2518ab6fc7557bf347c8e823c3d3
Eike <eike@denise.com>
2023-10-15 14:42:15 +0200

    Add solution for exercise 1

--- src/components/ScrumEstimation.vue
+++ src/components/ScrumEstimation.vue
@@ -1,11 +1,22 @@
 <template>
-
+  <div class="container">
+    <h1 class="funnyHeadline">This is fun</h1>
+    <div class="funnyText">
+      {{ funnyText }}
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-
+<script lang="ts" setup>
+const funnyText = "this even more";
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.funnyHeadline {
+  color: red;
+  font-size: 20px;
+}
+.container {
+  text-align: center;
+}
 </style>
