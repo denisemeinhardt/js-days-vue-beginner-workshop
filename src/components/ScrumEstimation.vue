01b93463fae0c015126ec45dc447f50f5f9e61e4
Eike <eike@denise.com>
2023-10-15 14:49:30 +0200

    Add solution for exercise 2

--- src/components/ScrumEstimation.vue
+++ src/components/ScrumEstimation.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="container">
     <h1 class="funnyHeadline">This is fun</h1>
-    <div class="funnyText">
+    <div class="funnyText" :title="anotherFunnyText">
       {{ funnyText }}
     </div>
+    <button @click="resetText('lustiger')">Change funny text</button>
+    <div>
+      {{ anotherFunnyText + " - " + funnyText }}
+    </div>
+    <button @click="resetText('unlustiger')">
+      Change funny text differently
+    </button>
   </div>
 </template>
 
 <script lang="ts" setup>
-const funnyText = "this even more";
+import { ref } from "vue";
+const funnyText = ref<string>("this even more");
+const anotherFunnyText = ref<string>("hello again");
+const resetText = (insertParameterHere: string) => {
+  funnyText.value = insertParameterHere + " wirds nicht mehr";
+};
 </script>
 
 <style lang="scss" scoped>
